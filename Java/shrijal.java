b189a99a14548b57cf7f718f6e15c9fc2e1326b4
Jagrit <44747746+Jagrit29@users.noreply.github.com>
2020-05-14 22:47:38 +0530

    Update shrijal.java

--- Java/shrijal.java
+++ Java/shrijal.java
@@ -1,4 +1,58 @@
-public static boolean search(Node head,int x)
+package Codes;
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int data)
+    {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class Code1
+{
+    static Node insertNode(Node head, int x)
+    {
+        Node node = new Node(x);
+        if(head == null)
+            return new Node(x);
+
+        Node temp = head;
+        while(temp.next != null)
+            temp = temp.next;
+
+        temp.next = node;
+
+        return head;
+    }
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+
+        while(t-- > 0)
+        {
+            int n = Integer.parseInt(read.readLine());
+
+            String str[] = read.readLine().trim().split(" ");
+            Node head = null;
+            for(int i = 0; i < n; i++)
+                head = insertNode(head, Integer.parseInt(str[i]));
+
+            int node = Integer.parseInt(read.readLine());
+
+            System.out.println(search(head, node) ? "1" : "0");
+        }
+    }
+
+		public static boolean search(Node head,int x)
 		{
 			//Empty LinkedList
 			if(head==null)
@@ -54,3 +108,5 @@ public static boolean search(Node head,int x)
 			
 			
 		}
+
+	}
